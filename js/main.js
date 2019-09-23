a94cfe6049331f35fb05c58c2995ae012fefe6e8
mustbebuilt <martin@mustbebuilt.co.uk>
2019-09-23 14:39:20 +0100

    2019/20 Revision

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-$('.burgerMenu').on('click', function(ev){
-	ev.preventDefault();
-	$(this).toggleClass('animateBurger');
-	$('nav').slideToggle('fast');
+(function(){
+$(".burgerMenu").on("click", function(ev) {
+  ev.preventDefault();
+  $(this).toggleClass("animateBurger");
+  $(".mainNav").slideToggle("fast");
 });
-$(window).on('resize', function(ev){
-	//console.info(window.innerWidth);
-	if(window.innerWidth > 600){
-		$('nav ul').attr('style','');	
-	};
+$(window).on("resize", function(ev) {
+  //console.info(window.innerWidth);
+  if (window.innerWidth > 720) {
+    $("nav ul").attr("style", "");
+  }
 });
+function navHighlight(elem, home, active) {
+    var url = location.href.split('/'),
+        loc = url[url.length -1],
+        link = document.querySelectorAll(elem);
+    for (var i = 0; i < link.length; i++) {
+        var path = link[i].href.split('/'),
+            page = path[path.length -1];
+        if (page == loc || page == home && loc == '') {
+            link[i].parentNode.className += ' ' + active;
+            document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
+            }
+        }
+    }
+navHighlight('.mainNav ul li a', 'index.html', 'current'); /* menu link selector, home page, highlight class */
+})();
